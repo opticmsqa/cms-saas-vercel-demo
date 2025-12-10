5a4b9f6e8d06276ccd96124da3ad1947c5b119dc
Son Luong <son.luong@optimizely.com>
2025-12-10 13:48:37 +0700

    update
    update

--- apps/frontend/src/components/factory.ts
+++ apps/frontend/src/components/factory.ts
@@ -3,9 +3,22 @@ import { type ComponentFactory, DefaultComponentFactory, RichTextComponentDictio
 import cmsComponents from './cms'
 
 // Create the server factory, to be reused throughout the application
-export const factory : ComponentFactory = new DefaultComponentFactory()
+export const factory: ComponentFactory = new DefaultComponentFactory()
 factory.registerAll(RichTextComponentDictionary)
-factory.registerAll(cmsComponents)
+
+// Register all CMS components AND their "Item/" prefixed versions
+// This is needed because after adding Global Contract, Optimizely Graph returns 
+// content types with "Item/" prefix (e.g., "Item/Page/SimplePage" instead of "Page/SimplePage")
+cmsComponents.forEach(({ type, component }) => {
+    // Register original type
+    factory.register(type, component)
+
+    // Also register with "Item/" prefix to support Global Contract
+    const itemType = Array.isArray(type)
+        ? ['Item', ...type]
+        : `Item/${type}`
+    factory.register(itemType, component)
+})
 
 /**
  * Get the cached version of the Component Factory to use, this ensure that the
