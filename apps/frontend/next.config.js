adc30351c3d13f476aca5f11ac188d2175cc836a
Son Luong <son.luong@optimizely.com>
2025-12-16 13:49:44 +0700

    update code
    update code

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
             // Optimizely CMS
             {
                 protocol: 'https',
-                hostname: '*.cmstest.optimizely.com',
+                hostname: '*.cms.optimizely.com',
                 pathname: '/**'
             },
             // Optimizely Content Recommendations
